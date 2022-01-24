600bc20c9674415859ed54c40a46598b936713b4
Adam <adam.bialik85@gmail.com>
2022-01-24 10:07:44 +0100

    Added comments to default setup

--- setup/index.php
+++ setup/index.php
@@ -9,17 +9,20 @@ $handler = new PHPExpress\Http\Handler([
     "RESPONSE_FLAGS" => JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES //Flags describing how responses will be encoded to json
 ]);
 
-$handler->get("/wpis/+:id", function(Request $req, Response $res) {
+$handler->get("/", function($none, Response $response) { //Default page
+    $response->send("Main page");
+});
+
+$handler->get("/post/+:id", function(Request $req, Response $res) { //Route with args
     $res->send(["id" => $req->params["id"]]);
 });
 
-$handler->get("/cus/*", function($none, Response $res) {
-    $res->send("It works! <i>tada!</i>");
-});
-
-$handler->get("/*", function(Request $req, Response $res) {
+$handler->get("/*", function(Request $req, Response $res) { //Matches all paths
     $fullpath = $req->fullRequestPath;
     $res->setError(404, "Requested resource not found!")->send("404 page not found: \"$fullpath\"");
 });
 
-$handler->listen();
+
+$handler->listen(); //If you add Route after calling this function it won't work
+
+$handler->post("/test", function() {echo "test";}); // Does not work (try above listen())
