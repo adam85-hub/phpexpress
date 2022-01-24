eaf2e9235776d91435c5b4e457ac9fff0ee631ac
Adam <adam.bialik85@gmail.com>
2022-01-24 11:46:37 +0100

    getSubDirs() added!

--- phpexpress/fs/fs.php
+++ phpexpress/fs/fs.php
@@ -2,57 +2,26 @@
 
 namespace PHPExpress\FS;
 
-use Exception;
+require_once __DIR__ . "\\file.php";
 
 /**
- * File which you can write and read to
+ * Returns subfolders of given directory as array
+ * @param mixed $path Path to directory
+ * 
+ * @return array
  */
-class File {
-    private $path;
+function getSubDirs($path): array {    
+    $output = array();
+    if ($handle = opendir($path)) {
 
-    /**
-     * File constructor
-     * 
-     * @param mixed $path Path to file.
-     * @param bool $create If true file will be created if it does not exist
-     */
-    function __construct($path, bool $create = false)
-    {
-        $this->path = $path;
-
-        if(file_exists($path) == false) 
-        { 
-            if($create == true) {
-                file_put_contents($path, "");
-            } else {
-                throw new Exception("File $path does not exist!", 500);
+        while (false !== ($subDir = readdir($handle))) {
+            if($subDir != "." && $subDir != ".." && is_dir($path . "\\" . $subDir)) {
+                array_push($output, $subDir);
             }
         }
+
+        closedir($handle);
     }
-    /**
-     * Writes data to file.
-     * @param mixed $mixin Data that will be written to the file
-     * 
-     * @return void
-     */
-    public function WriteAll($mixin) {
-        file_put_contents($this->path, $mixin);
-    }
-    /**
-     * Reads all contents of file as string
-     * 
-     * @return string
-     */
-    public function ReadAllText(): string {
-        return file_get_contents($this->path);
-    }
-    /**
-     * Reads all contents of file as json
-     * 
-     * @return array
-     */
-    public function ReadAllJson(): array {
-        $jsonString = file_get_contents($this->path);
-        return json_decode($jsonString);
-    }
+
+    return $output;
 }
